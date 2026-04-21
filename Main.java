607609d62076cf4b4ea4047ae6815784ec7a67a1
orlandoquezada49-stack <orlandoquezada49@aragon.unam.mx>
2026-04-20 23:42:10 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package git_interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        miPrimerGit miPrimerGit = new HolaMundo();
+        miPrimerGit.mensaje();
+    }
+}
---
+++ miPrimerGit.java
@@ -0,0 +1,5 @@
+package git_interfaces;
+
+interface miPrimerGit {
+    void mensaje();
+}
